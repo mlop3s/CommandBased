21915a0cc83ed373f081e2ce3304a21f642973e9
Lopes Marco <marco.lopes@nexus-ag.de>
2024-11-10 12:35:38 +0100

    SendMail

---
+++ CommandLineWebApp/Devops/SendMail.ps1
@@ -0,0 +1,24 @@
+﻿param (
+    [string]$buildId
+)
+
+$EmailTo = 'Marco.Lopes@nexus-ag.de'
+$EmailFrom = "azurebot@alemaes.de"
+$Subject = "Webservice Docs" 
+$Body = "
+Your webservice has been deployed
+"
+$pathToFile="$env:BUILD_ARTIFACTSTAGINGDIRECTORY/docs_$buildId.zip"
+
+Write-Output $pathToFile
+
+$SMTPServer = "w0139d43.kasserver.com"
+$SMTPPort = 587 # Port for TLS
+$SMTPMessage = New-Object System.Net.Mail.MailMessage($EmailFrom, $EmailTo, $Subject, $Body)
+$attachment = New-Object System.Net.Mail.Attachment($pathToFile)
+$SMTPMessage.Attachments.Add($attachment)
+$SMTPClient = New-Object Net.Mail.SmtpClient($SMTPServer, $SMTPPort)
+$SMTPClient.EnableSsl = $true # Enable SSL for TLS
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($EmailFrom, $env:EmailPassword)
+Write-Output $Body
+$SMTPClient.Send($SMTPMessage)
